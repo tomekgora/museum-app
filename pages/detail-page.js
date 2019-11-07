98cc13714b159c4e5c6fb36acc8a96f16f3fa538
tomekgora <55657311+tomekgora@users.noreply.github.com>
2019-11-07 14:28:30 +0100

    Update detail-page.js
    Resolving conflict

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,13 +1,31 @@
+
+// First validate the inupt 
+function doesNotPassAllValidations(name, msg) {
+    if (!name || !msg) {
+      alert('You forgot to fill in your name or message!')
+      return true;
+    }
+  
+    if(msg.length > 280) {
+      alert('Your comment is too long')
+      return true
+    }
+  
+    return false
+  }
+
+
+
 function submitComment () {
-    console.log("Hello, the button was pushed");
     const inputField = document.getElementById('name');
-    console.log(inputField);
     const name = inputField.value;
-    console.log(name);
     const textArea = document.getElementById('msg');
     const msg = textArea.value
-    console.log(msg)
 
+    if(doesNotPassAllValidations(name, msg)){
+        return null
+    }
+    
     //Creating the needed elements
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
@@ -19,14 +37,13 @@ function submitComment () {
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
-    console.log(comment);
 
-    // display elements on the page
-    const commentSection = document.getElementById('comments');
+    // display the elements on the page
+    const commentSection = document.getElementById("comments");
     commentSection.appendChild(comment);
 
-    //reset the values in the form
-    inputField.value = null;
-    textArea.value = null;
-    
+  // reset form values
+  inputField.value = null;
+  textArea.value = null;
 }
+
