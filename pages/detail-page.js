b4f661abd2cc4fec70228f4a64e27838955bf430
tomekgora <tomekgora@gmail.com>
2019-11-07 12:33:40 +0100

    Create elements with form data and CSS ffor detail-page

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,23 @@
 function submitComment () {
     console.log("Hello, the button was pushed");
+    const inputField = document.getElementById('name');
+    console.log(inputField);
+    const name = inputField.value;
+    console.log(name);
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value
+    console.log(msg)
+
+    //Creating the needed elements
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p')
+
+    //Adjusting the created elements
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
 }
